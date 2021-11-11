e81bf7fa63ea060dea3f599da429a3f82c0f8a07
chtulur <molnar.gly@gmail.com>
2021-11-11 22:24:50 +0100

    Done

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,6 @@
-
-
-
-
-
-
-// export default setCookie;
+const setCookie = (str) => {
+  const now = new Date();
+  const expiry = now.setTime(now.getTime() + 900000);
+  return (document.cookie = `token=${str}; expires=${expiry}+ ${expiry}`);
+};
+export default setCookie;
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,38 @@
+const setCookie = () => {
+  const now = new Date();
+  const expiry = now.setTime(now.getTime() + 900000);
+  return (document.cookie = `viewed=5; expires=${expiry}+ ${expiry}`);
+};
+const setCookie2 = () => {
+  const now = new Date();
+  const expiry = now.setTime(now.getTime() + 900000);
+  return (document.cookie = `uid=354774631237; expires=${expiry}+ ${expiry}`);
+};
+const setCookie3 = () => {
+  const now = new Date();
+  const expiry = now.setTime(now.getTime() + 900000);
+  return (document.cookie = `ssid=Bx55OWbHJ0Vt_IGIF; expires=${expiry}+ ${expiry}`);
+};
 
+setCookie();
+setCookie2();
+setCookie3();
 
-
-
-
-
-// export { cookieHandler };
+const cookieHandler = {
+  getAll() {
+    const myStr = document.cookie.split("; ").map((item) => item.split("="));
+    return Object.fromEntries(myStr);
+  },
+  toSessionStorage() {
+    const myStr = document.cookie.split("; ").map((item) => item.split("="));
+    myStr.forEach((item) => sessionStorage.setItem(item[0], item[1]));
+  },
+  flush() {
+    const myStr = document.cookie.split("; ").map((item) => item.split("="));
+    myStr.forEach(
+      (item) =>
+        (document.cookie = `${item[0]}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`)
+    );
+  },
+};
+export { cookieHandler };
